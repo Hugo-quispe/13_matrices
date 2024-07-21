6c090f0491f4f0cd11a9301bbf4db38a1cd1e116
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-20 22:22:50 -0500

    se completo el ejercicio 2 correctamente y tambien se añadio una funcion adicional al programa principal la cual es imprimir la matriz

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+    int n,s=0;
+    cout<<"ingrese el orden de la matriz: ";cin>>n;
+    int matriz[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"ingrese en numero de la posicion ["<<i+1<<"]["<<j+1<<"]: ";cin>>matriz[i][j];
+            if(i>j){
+                s=s+matriz[i][j];
+            }
+        }
+    }
+    cout<<"la suma de los numeros pertenecientes a la parte inferior de la diagonal principal es: "<<s<<endl;
+    cout<<"matriz generada: "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<matriz[i][j];
+        }
+        cout<<"\n";
+    }
 }
