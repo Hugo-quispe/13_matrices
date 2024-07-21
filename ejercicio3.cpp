51624d75d983278a110ce7de3c4f60f5a3b9c7b7
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-20 23:48:28 -0500

    se genero la matriz identidad pero aun no se logra lo que pide el ejercicio 3

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int n;
+    cout<<"ingrese el orden de la matriz: ";cin>>n;
+    int matriz[n][3*n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3*n;j++){
+            if(i<n&&j<3*n){
+                matriz[i][j]=1;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3+n;j++){
+            cout<<matriz[i][j];
+        }
+        cout<<"\n";
+    }
 }
