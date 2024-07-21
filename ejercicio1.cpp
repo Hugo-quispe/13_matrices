930dc7937af7b5666851b0f2b6b5d7847ae27793
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-20 21:37:37 -0500

    se completo el ejercicio 1 exitosamente

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int n,s=0;
+    cout<<"ingrese el orden de la matriz: ";cin>>n;
+    int matriz[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"ingrese los valores de las posiciones ["<<i+1<<"]"<<"["<<j+1<<"]:";cin>>matriz[i][j];
+            if(i<j){
+                s=s+matriz[i][j];
+            }
+        }
+    }
+    cout<<s;
+    return 0;
 }
